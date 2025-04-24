4851b82d941e313df3b5f61b363b1ade86bb47e3
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-24 13:40:24 +0530

    Update JobRepositoryImpl.cs

--- JobPortalBackend/Repositories/impl/JobRepositoryImpl.cs
+++ JobPortalBackend/Repositories/impl/JobRepositoryImpl.cs
@@ -1,6 +1,12 @@
+using JobPortalBackend.Data;
+
 namespace JobPortalBackend.Repositories.impl;
 
 public class JobRepositoryImpl: IJobRepository
 {
-    
+    private readonly AppDbContext _context;
+    public JobRepositoryImpl(AppDbContext context)
+    {
+        _context = context;
+    }
 }

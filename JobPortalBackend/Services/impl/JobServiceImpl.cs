b28e334f109d58899c43996a37e0bd42dabb400f
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-25 00:05:17 +0530

    Update JobServiceImpl.cs

--- JobPortalBackend/Services/impl/JobServiceImpl.cs
+++ JobPortalBackend/Services/impl/JobServiceImpl.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using JobPortalBackend.Dtos.request;
+using JobPortalBackend.Dtos.response;
+using JobPortalBackend.Models;
 using JobPortalBackend.Repositories;
 
 namespace JobPortalBackend.Services.impl;
@@ -5,8 +9,18 @@ namespace JobPortalBackend.Services.impl;
 public class JobServiceImpl: IJobService
 {
     private readonly IJobRepository _jobRepository;
-    public JobServiceImpl(IJobRepository jobRepository)
+    private readonly IMapper _mapper;
+    public JobServiceImpl(IJobRepository jobRepository , IMapper mapper)
     {
         _jobRepository = jobRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<JobDtoResponse> CreateJob(JobDtoRequest jobDto)
+    {
+        Job job = _mapper.Map<Job>(jobDto);
+        Job createdJob = await _jobRepository.CreateJob(job);
+        JobDtoResponse jobDtoResponse = _mapper.Map<JobDtoResponse>(createdJob);
+        return jobDtoResponse;
     }
 }

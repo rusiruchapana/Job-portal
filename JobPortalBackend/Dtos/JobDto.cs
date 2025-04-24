f1f8e29fbf3aead27c2c939831fcd57671540398
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-25 00:05:40 +0530

    Delete JobDto.cs

--- JobPortalBackend/Dtos/JobDto.cs
+++
@@ -1,9 +0,0 @@
-namespace JobPortalBackend.Dtos;
-
-public class JobDto
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Company { get; set; }
-    public string Location { get; set; }
-}

ff4257fa20511e3ad076fa8ba42e99c099920929
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-24 13:40:27 +0530

    Update JobServiceImpl.cs

--- JobPortalBackend/Services/impl/JobServiceImpl.cs
+++ JobPortalBackend/Services/impl/JobServiceImpl.cs
@@ -1,6 +1,12 @@
+using JobPortalBackend.Repositories;
+
 namespace JobPortalBackend.Services.impl;
 
 public class JobServiceImpl: IJobService
 {
-    
+    private readonly IJobRepository _jobRepository;
+    public JobServiceImpl(IJobRepository jobRepository)
+    {
+        _jobRepository = jobRepository;
+    }
 }

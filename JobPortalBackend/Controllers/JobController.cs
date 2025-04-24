0f5351399296704503255303b982c650e55a8db8
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-24 13:46:51 +0530

    Update JobController.cs

--- JobPortalBackend/Controllers/JobController.cs
+++ JobPortalBackend/Controllers/JobController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JobPortalBackend.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class JobController: ControllerBase
 {
     private readonly IJobService _jobService;

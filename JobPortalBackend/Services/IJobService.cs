aa294356aceac99adee8265a8f8f8bc5ec184d23
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-25 00:05:14 +0530

    Update IJobService.cs

--- JobPortalBackend/Services/IJobService.cs
+++ JobPortalBackend/Services/IJobService.cs
@@ -1,6 +1,9 @@
+using JobPortalBackend.Dtos.request;
+using JobPortalBackend.Dtos.response;
+
 namespace JobPortalBackend.Services;
 
 public interface IJobService
 {
-    
+    Task<JobDtoResponse> CreateJob(JobDtoRequest jobDto);
 }

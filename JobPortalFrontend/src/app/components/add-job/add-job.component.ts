05ac8f6ce276bd4ac7475d558afe19b4a390c11b
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-26 09:56:54 +0530

    generate components that needed.

---
+++ JobPortalFrontend/src/app/components/add-job/add-job.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-job',
+  imports: [],
+  templateUrl: './add-job.component.html',
+  styleUrl: './add-job.component.scss'
+})
+export class AddJobComponent {
+
+}

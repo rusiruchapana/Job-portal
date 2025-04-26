731e886a40b496520157dde0e52f49be17ce7e32
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-04-26 10:52:18 +0530

    add routes.

--- JobPortalFrontend/src/app/app.routes.ts
+++ JobPortalFrontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { AddJobComponent } from './components/add-job/add-job.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'/jobs', pathMatch: 'full'},
+    {path:'jobs', component: JobListComponent},
+    {path:'jobs/add', component: AddJobComponent},
+    {path:'jobs/:id', component: JobDetailsComponent}
+ ];
